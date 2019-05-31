514bc907e2e4d809993b1f16645adfccc28474b6
vgybalo <vgybalo@ukr.net>
2019-05-31 19:13:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let vertical_road = document.querySelector('.vertical-road');
+let horisontal_road = document.querySelector('.horisontal-road');
+let green_light_horisontal = document.querySelector('.roadlight_vertical');
+let red_light_horisontal = document.querySelector('.roadlight_horisontal');
+let green_light_vertical = document.querySelector('.roadlight_vertical');
+let red_light_vertical = document.querySelector('.roadlight_horisontal');
+let button = document.querySelector('.button');
+
+button.addEventListener('mouseover', function() {
+    red_light_horisontal.innerHTML.style.backgroundColor = grey;
+
+}
+
