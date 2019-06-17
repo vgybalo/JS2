9bdd45b6d2941493f292e666abc37c8f0324f01f
vgybalo <vgybalo@ukr.net>
2019-06-18 01:30:20 +0300

    fourrteenth commit

--- main.js
+++ main.js
@@ -1,38 +1,51 @@
 let square1 = document.querySelector('.square1');
+let square_big = document.querySelector('.square_big');
 let coord_x = 0;
 let coord_y = 0;
-
-window.addEventListener('mouseover', move );
-
-function move (e) {
-    coord_x = Number(e.pageX);
-    coord_y = Number(e.pageY);
-    square1.style.top = (coord_y + delta_top) + 'px';
-    square1.style.left = (coord_x + delta_left) + 'px';
-}
-
-window.removeEventListener('mouseup', move);
-
+let mouse_button_press = 0;
 
 let coordinates_square1 = square1.getBoundingClientRect();
 let top_square1 = Number(coordinates_square1.top);
 let left_square1 =  Number(coordinates_square1.left);
+let bottom_square1 = Number(coordinates_square1.bottom);
+let right_square1 =  Number(coordinates_square1.right);
 
-let delta_top = coord_y - top_square1;
-let delta_left = coord_x - left_square1;
+let coordinates_square_big = square_big.getBoundingClientRect();
+let top_square_big = Number(coordinates_square_big.top);
+let bottom_square_big = Number(coordinates_square_big.bottom);
+let left_square_big =  Number(coordinates_square_big.left);
+let right_square_big =  Number(coordinates_square_big.right);
 
-console.log(top_square1);
-console.log(left_square1);
+let delta_top = bottom_square1 - top_square1;
+let delta_left = right_square1 - left_square1;
+
+/*square_big.addEventListener('mousedown', function() {
+    square_big.addEventListener('mouseover', move );
+    square_big.removeEventListener('mouseup', move);
+});*/
+
+
+function move (e) {
+    mouse_button_press = e.button;
+    coord_x = e.offsetX;
+    coord_y = e.offsetY;
+
+   if (left_square1 >= left_square_big && right_square1 <= right_square_big &&
+       top_square1 >= top_square_big && bottom_square1 <= bottom_square_big){
+
+        square1.style.top = (coord_y - delta_top/2 ) + 'px';
+        square1.style.left = (coord_x - delta_left/2 ) + 'px';}
 
 console.log(coord_x);
 console.log(coord_y);
+    console.log( square1.style.top);
+    console.log(square1.style.left);
+}
 
+square_big.addEventListener('mousemove', move );
 
-
-
-
-
-
+console.log(delta_top);
+console.log(delta_left);
 
 
 
